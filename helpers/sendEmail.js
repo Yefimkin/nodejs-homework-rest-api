5d26699620f4e7e99ac40c672552625328ac5516
Yefimkin Denys <iefimkindenis@gmail.com>
2023-01-22 20:16:59 +0200

    Update sendEmail.js

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,20 +1,35 @@
+// const sgMail = require("@sendgrid/mail");
+
+// sgMail.setApiKey(process.env.SEND_GRIP_API_KEY);
+// const sendEmail = {
+//   to: "xapoja4937@fom8.com",
+//   from: "xapoja4937@fom8.com",
+//   subject: "Sending with SendGrid is Fun",
+//   text: "and easy to do anywhere, even with Node.js",
+//   html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+// };
+// sgMail
+//   .send(sendEmail)
+//   .then(() => {
+//     console.log("Email sent");
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
+
+// module.exports = sendEmail;
+
 const sgMail = require("@sendgrid/mail");
-// const { SEND_GRIP_API_KEY } = require("../token");
-sgMail.setApiKey(process.env.SEND_GRIP_API_KEY);
-const sendEmail = {
-  to: "xapoja4937@fom8.com",
-  from: "xapoja4937@fom8.com",
-  subject: "Sending with SendGrid is Fun",
-  text: "and easy to do anywhere, even with Node.js",
-  html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+require("dotenv").config();
+
+const { SEND_GRIP_API_KEY } = process.env;
+
+sgMail.setApiKey(SEND_GRIP_API_KEY);
+
+const sendEmail = async (data) => {
+  const email = { ...data, from: "xapoja4937@fom8.com" };
+  await sgMail.send(email);
+  return true;
 };
-sgMail
-  .send(sendEmail)
-  .then(() => {
-    console.log("Email sent");
-  })
-  .catch((error) => {
-    console.error(error);
-  });
 
 module.exports = sendEmail;
