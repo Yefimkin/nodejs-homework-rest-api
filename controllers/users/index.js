f152e741f3181e04e41575a91844a406e1578f64
Yefimkin Denys <iefimkindenis@gmail.com>
2023-01-22 17:47:03 +0200

    Update index.js

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -4,6 +4,8 @@ const logout = require("./logout");
 const getCurrent = require("./getCurrent");
 const updateSubscription = require("./updateSubscription");
 const updateAvatar = require("./updateAvatar");
+const verifyEmail = require("./verifyEmail");
+const sendVerifyEmail = require("./sendVerifyEmail");
 
 module.exports = {
   register,
@@ -12,4 +14,6 @@ module.exports = {
   getCurrent,
   updateSubscription,
   updateAvatar,
+  verifyEmail,
+  sendVerifyEmail,
 };
