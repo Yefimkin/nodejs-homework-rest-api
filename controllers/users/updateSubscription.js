35d4115eaa3ef0182c607c0874c09f6003e99506
Yefimkin Denys <iefimkindenis@gmail.com>
2023-01-22 20:04:27 +0200

    Update updateSubscription.js

--- controllers/users/updateSubscription.js
+++ controllers/users/updateSubscription.js
@@ -2,11 +2,6 @@ const { User } = require("../../models");
 
 const updateSubscription = async (req, res) => {
   const userId = req.user._id;
-  const userPassword = req.body.password;
-  if (userPassword) {
-    // eslint-disable-next-line no-undef
-    req.body.password = bcrypt.hashSync(userPassword, bcrypt.genSaltSync(10));
-  }
 
   const result = await User.findOneAndUpdate({ userId }, { ...req.body });
   res.json({
