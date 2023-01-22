7d621c9abab9e61ea7bb8a2417a8962988f5f6e7
Yefimkin Denys <iefimkindenis@gmail.com>
2023-01-22 17:46:55 +0200

    Update updateSubscription.js

--- controllers/users/updateSubscription.js
+++ controllers/users/updateSubscription.js
@@ -2,7 +2,14 @@ const { User } = require("../../models");
 
 const updateSubscription = async (req, res) => {
   const { _id } = req.user;
-  const result = await User.findByIdAndUpdate(_id, { ...req.body });
+  const { email, password, subscription } = req.body;
+  const newUser = new User({
+    email,
+    subscription,
+  });
+  newUser.setPassword(password);
+  newUser.save();
+  const result = await User.findByIdAndUpdate(_id, { ...newUser });
   res.json({
     status: "success",
     code: 200,
